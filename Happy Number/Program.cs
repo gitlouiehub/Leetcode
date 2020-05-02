e04124b327b43999e1b948d31936ac6ed735c5f3
Louie Munoz <munozlu.f@live.com>
2020-05-02 12:51:11 -0700

    Add project files.

---
+++ Happy Number/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Happy_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsHappy(1111111));
+        }
+
+        public static bool IsHappy(int n)
+        {
+            const int HAPPY_NUMBER = 1;
+            bool isHappy = false;
+            int sum = 0; // extracts first digit
+            int extractMostRightDigit = n;
+            int postExtraction = n;
+
+            while (postExtraction != 0)
+            {
+
+                extractMostRightDigit %= 10;
+                sum += (int)Math.Pow(extractMostRightDigit, 2);
+
+                Console.WriteLine(sum);
+                postExtraction /= 10;
+                extractMostRightDigit = postExtraction;
+
+                if (postExtraction == 0)
+                {
+                    extractMostRightDigit = sum;
+                    postExtraction = sum;
+                    sum = 0;
+                    if (postExtraction == HAPPY_NUMBER)
+                    {
+                        isHappy = true;
+                        break;
+                    }
+                    else if (postExtraction != HAPPY_NUMBER)
+                    {
+                        isHappy = false;
+                        break;
+                    }
+                }
+
+            }
+            return isHappy;
+        }
+    }
+}
---
+++ Leetcode/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Text;
+
+namespace Leetcode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(SingleNumber(new int[] { 4, 1, 2, 1, 2 }));
+
+        }
+        public static int SingleNumber(int[] nums)
+        {
+
+
+            int temp = 0;
+            int counter = 0; // 
+            for (int j = 0; j < nums.Length; j++) // iterate the length of the whole array
+            {
+                // swap element to be tested at position it to the beginning
+                temp = nums[0]; // 
+                nums[0] = nums[j]; //
+                nums[j] = temp;
+                for (int i = 1; i < nums.Length; i++) // test the first element with the rest of the array
+                {
+                    if (nums[0] == nums[i]) // if we find a number equal to each other, 
+                    {
+                        // swap back to it's original array
+                        temp = nums[j]; 
+                        nums[j] = nums[0]; //
+                        nums[0] = temp;
+                        counter = 0;
+                        break; // out of inner loop
+                    }
+                    counter++;
+                }
+                if (counter == nums.Length) // if the counter equals the length then that element passed
+                {
+                    break; // out of outer loop
+                }
+            }
+            
+            return nums[0];
+        }
+
+    }
+}
