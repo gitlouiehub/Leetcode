481f92712fbb074a49088308c69d6d6a93fecf86
Louie Munoz <munozlu.f@live.com>
2020-05-03 11:26:23 -0700

    Added new programs

---
+++ Jewels and Stones/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Jewels_and_Stones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(NumJewelsInStones("bcd", "cbs"));
+        }
+
+        public static int NumJewelsInStones(string J, string S)
+        {
+            int count = 0;
+            if (S.Length <= 50 & J.Length <= 50)
+            {
+                for (int i = 0; i < J.Length; i++)
+                {
+                    string s = J.Substring(i, 1);
+                    while (S.Contains(J.Substring(i, 1)))
+                    {
+                        count++;
+                        S = S.Remove(S.IndexOf(s), 1);
+                    }
+                }
+            }
+            else
+            {
+                count = 0;
+            }
+            return count;
+        }
+    }
+}
---
+++ Ransom Note/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Ransom_Note
+{
+    /**
+     * Given an      ransom note string and another string containing letters from all the magazines, write a function that will return true if the ransom note can be constructed from the magazines ; otherwise, it will return false. 
+    Each letter in the magazine string can only be used once in your ransom note. 
+Note:
+You may assume that both strings contain only lowercase letters. 
+
+     * */
+    // canConstruct("a", "b") -> false
+    // canConstruct("aa", "ab") -> false
+    // canConstruct("aabb", "aab") -> true
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CanConstruct("fffbfg", "effjffggbffjdgbjjhhdegh"));
+        }
+
+        public static bool CanConstruct(string ransomNote, string magazine)
+        {
+            bool canConstruct = false;
+            int count = 0;
+            foreach (char ch in ransomNote.ToCharArray())
+            {
+                if (magazine.Contains(ch))
+                {
+                    count++;
+                    magazine = magazine.Remove(magazine.IndexOf(ch), 1);
+                }
+            }
+            if (count == ransomNote.Length)
+            {
+                canConstruct = true;
+            }
+            else
+            {
+                canConstruct = false;
+            }
+            return canConstruct;
+        }
+    }
+}
