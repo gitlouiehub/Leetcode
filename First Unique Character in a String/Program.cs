557f9de86419be486c2a92c16f4d1950d084e680
Louie Munoz <munozlu.f@live.com>
2020-05-05 16:13:54 -0700

    First Unique Character In A String

---
+++ First Unique Character in a String/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace First_Unique_Character_in_a_String
+{
+    /*
+     * 
+     * Given a string, find the first non-repeating character in it and return it's index. 
+     * If it doesn't exist, return -1. 
+     * */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FirstUniqChar("aadadaadc"));
+        }
+        public static int FirstUniqChar(string s)
+        {
+            int pos = -1;
+            int length = s.Length;
+            
+            if (length == 1)
+                pos = 0;
+            else if(string.IsNullOrEmpty(s))
+            {
+                pos = -1;
+            }
+            else
+            {
+                HashSet<char> container = new HashSet<char>();
+                for (int i = 0; i < length; i++)
+                {
+                    if (!container.Contains(s[i]) && !s.Substring(i+1).Contains(s[i]))
+                    {
+                        pos = s.IndexOf(s[i]);
+                        break;
+                    }
+                    else
+                    {
+                        container.Add(s[i]);
+                    }
+                }
+            }
+            return pos;
+        }
+    }
+}
